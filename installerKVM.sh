46641403ce93a5c5786345dc3a107d2a343479d3
AidikaAA <mitrassh10@gmail.com>
2017-07-08 18:31:16 +0700

    Update installerKVM.sh

--- installerKVM.sh
+++ installerKVM.sh
@@ -252,8 +252,8 @@ chmod +x speedtest.py
 
 # Install Menu
 cd
-wget "https://raw.githubusercontent.com/AidikaAA/autovps/master/a"
-mv ./a /usr/local/bin/menu
+wget "https://raw.githubusercontent.com/AidikaAA/autovps/master/s"
+mv ./s /usr/local/bin/menu
 chmod +x /usr/local/bin/menu
 
 # swap ram
