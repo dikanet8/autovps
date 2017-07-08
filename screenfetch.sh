51f36d61271b0c6bb0449849e733180d1b3e9087
AidikaAA <mitrassh10@gmail.com>
2017-07-08 15:45:12 +0700

    Update and rename screenfetch.sh to screenfetch

--- screenfetch.sh
+++
@@ -1,6 +0,0 @@
- wget "https://github.com/KittyKatt/screenFetch/archive/master.zip"
- unzip master.zip
- mv screenFetch-master/screenfetch-dev /usr/bin
- cd /usr/bin
- sudo mv screenfetch-dev screenfetch
- chmod 755 screenfetch
